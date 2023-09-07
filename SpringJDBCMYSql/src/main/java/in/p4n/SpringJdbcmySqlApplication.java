e8434de218c27134ee782e141e2184eb807daeb4
SanjanaMore7 <135117348+SanjanaMore7@users.noreply.github.com>
2023-09-07 16:22:27 +0530

    Add files via upload

---
+++ SpringJDBCMYSql/src/main/java/in/p4n/SpringJdbcmySqlApplication.java
@@ -0,0 +1,13 @@
+package in.p4n;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJdbcmySqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringJdbcmySqlApplication.class, args);
+	}
+
+}
