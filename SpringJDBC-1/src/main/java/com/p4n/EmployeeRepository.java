6429a1e49f7181fd3786a806b454d245cd598a99
SanjanaMore7 <135117348+SanjanaMore7@users.noreply.github.com>
2023-09-09 15:19:41 +0530

    Add files via upload

---
+++ SpringJDBC-1/src/main/java/com/p4n/EmployeeRepository.java
@@ -0,0 +1,9 @@
+package com.p4n;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
+
+}
